7ca1edfdd9c5b4ac1df8dc30660ba86fd8124702
Pilar Mingueza <74919850+pmh-itacademy@users.noreply.github.com>
2021-03-28 19:55:49 +0200

    Correccions relacions n : n

--- Youtube/youtube.sql
+++ Youtube/youtube.sql
@@ -16,14 +16,6 @@ CREATE TABLE IF NOT EXISTS `usuari` (
   PRIMARY KEY (`Usuari_id`));
 
 -- -----------------------------------------------------
--- Table `estat_video`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `estat_video` (
-  `estat_video_id` TINYINT NOT NULL,
-  `estat_video_desc` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`estat_video_id`));
-
--- -----------------------------------------------------
 -- Table `playlist`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `playlist` (
@@ -34,8 +26,16 @@ CREATE TABLE IF NOT EXISTS `playlist` (
   `usuari_Usuari_id` INT NOT NULL,
   PRIMARY KEY (`playlist_id`),
   INDEX `fk_playlist_usuari1_idx` (`usuari_Usuari_id` ASC) VISIBLE,
-    FOREIGN KEY (`usuari_Usuari_id`)
+  FOREIGN KEY (`usuari_Usuari_id`)
     REFERENCES `usuari` (`Usuari_id`));
+
+-- -----------------------------------------------------
+-- Table `estat_video`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `estat_video` (
+  `estat_video_id` TINYINT NOT NULL,
+  `estat_video_desc` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`estat_video_id`));
 
 -- -----------------------------------------------------
 -- Table `video`
@@ -51,15 +51,15 @@ CREATE TABLE IF NOT EXISTS `video` (
   `video_num_reproduccions` INT NOT NULL,
   `video_num_likes` INT NOT NULL,
   `video_num_dislikes` INT NOT NULL,
-  `video_estat` TINYINT NULL COMMENT "1: públic  2: ocult  3: privat",
-  `video_hora_publ` DATETIME NULL,
+  `video_hora_publ` TIME NULL,
   `usuari_Usuari_id` INT NOT NULL,
+  `estat_video_id` TINYINT NOT NULL,
   PRIMARY KEY (`video_id`),
   INDEX `fk_video_usuari1_idx` (`usuari_Usuari_id` ASC) VISIBLE,
-  INDEX `fk_video_estat_video1_idx` (`video_estat` ASC) VISIBLE,
+  INDEX `fk_video_estat_video1_idx` (`estat_video_id` ASC) VISIBLE,
   FOREIGN KEY (`usuari_Usuari_id`)
     REFERENCES `usuari` (`Usuari_id`),
-  FOREIGN KEY (`video_estat`)
+  FOREIGN KEY (`estat_video_id`)
     REFERENCES `estat_video` (`estat_video_id`));
 
 -- -----------------------------------------------------
@@ -68,11 +68,7 @@ CREATE TABLE IF NOT EXISTS `video` (
 CREATE TABLE IF NOT EXISTS `etiqueta` (
   `etiqueta_id` INT NOT NULL AUTO_INCREMENT,
   `etiqueta_nom` VARCHAR(45) NOT NULL,
-  `video_video_id` INT NOT NULL,
-  PRIMARY KEY (`etiqueta_id`, `etiqueta_nom`),
-  INDEX `fk_etiqueta_video1_idx` (`video_video_id` ASC) VISIBLE,
-  FOREIGN KEY (`video_video_id`)
-    REFERENCES `video` (`video_id`));
+  PRIMARY KEY (`etiqueta_id`));
 
 -- -----------------------------------------------------
 -- Table `canal`
@@ -82,10 +78,10 @@ CREATE TABLE IF NOT EXISTS `canal` (
   `canal_nom` VARCHAR(30) NOT NULL,
   `canal_descripcio` VARCHAR(400) NULL,
   `canal_data_creacio` DATE NOT NULL,
-  `usuari_Usuari_id` INT NOT NULL,
+  `usuari_crea_id` INT NOT NULL,
   PRIMARY KEY (`canal_id`),
-  INDEX `fk_canal_usuari1_idx` (`usuari_Usuari_id` ASC) VISIBLE,
-  FOREIGN KEY (`usuari_Usuari_id`)
+  INDEX `fk_canal_usuari1_idx` (`usuari_crea_id` ASC) VISIBLE,
+  FOREIGN KEY (`usuari_crea_id`)
     REFERENCES `usuari` (`Usuari_id`));
 
 -- -----------------------------------------------------
@@ -93,13 +89,13 @@ CREATE TABLE IF NOT EXISTS `canal` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `canal_usuario` (
   `canal_canal_id` INT NOT NULL,
-  `usuari_Usuari_id` INT NOT NULL,
-  PRIMARY KEY (`canal_canal_id`, `usuari_Usuari_id`),
-  INDEX `fk_canal_usuario_usuari1_idx` (`usuari_Usuari_id` ASC) INVISIBLE,
+  `usuari_suscrit_id` INT NOT NULL,
+  PRIMARY KEY (`canal_canal_id`, `usuari_suscrit_id`),
+  INDEX `fk_canal_usuario_usuari1_idx` (`usuari_suscrit_id` ASC) INVISIBLE,
   INDEX `fk_canal_canal_id` (`canal_canal_id` ASC) VISIBLE,
   FOREIGN KEY (`canal_canal_id`)
     REFERENCES `canal` (`canal_id`),
-  FOREIGN KEY (`usuari_Usuari_id`)
+  FOREIGN KEY (`usuari_suscrit_id`)
     REFERENCES `usuari` (`Usuari_id`));
 
 -- -----------------------------------------------------
@@ -108,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `canal_usuario` (
 CREATE TABLE IF NOT EXISTS `video_like` (
   `video_video_id` INT NOT NULL,
   `usuari_Usuari_id` INT NOT NULL,
-  `video_like_tipus` TINYINT NOT NULL COMMENT '0: dislike, 1:like',
+  `video_like_tipus` TINYINT NOT NULL,
   `video_like_data` DATETIME NOT NULL,
   PRIMARY KEY (`video_video_id`, `usuari_Usuari_id`),
   INDEX `fk_video_like_video1_idx` (`video_video_id` ASC) VISIBLE,
@@ -149,6 +145,34 @@ CREATE TABLE IF NOT EXISTS `comentari_like` (
     REFERENCES `comentari` (`comentari_id`));
 
 -- -----------------------------------------------------
+-- Table `video_etiqueta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `video_etiqueta` (
+  `video_video_id1` INT NOT NULL,
+  `etiqueta_etiqueta_id1` INT NOT NULL,
+  PRIMARY KEY (`video_video_id1`, `etiqueta_etiqueta_id1`),
+  INDEX `fk_video_etiqueta_video1_idx` (`video_video_id1` ASC) VISIBLE,
+  INDEX `fk_video_etiqueta_etiqueta1_idx` (`etiqueta_etiqueta_id1` ASC) VISIBLE,
+  FOREIGN KEY (`video_video_id1`)
+    REFERENCES `video` (`video_id`),
+  FOREIGN KEY (`etiqueta_etiqueta_id1`)
+    REFERENCES `etiqueta` (`etiqueta_id`));
+
+-- -----------------------------------------------------
+-- Table `video_playlist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `video_playlist` (
+  `playlist_playlist_id` INT NOT NULL,
+  `video_video_id` INT NOT NULL,
+  PRIMARY KEY (`playlist_playlist_id`, `video_video_id`),
+  INDEX `fk_video_playlist_playlist1_idx` (`playlist_playlist_id` ASC) VISIBLE,
+  INDEX `fk_video_playlist_video1_idx` (`video_video_id` ASC) VISIBLE,
+  FOREIGN KEY (`playlist_playlist_id`)
+    REFERENCES `playlist` (`playlist_id`),
+  FOREIGN KEY (`video_video_id`)
+    REFERENCES `video` (`video_id`));
+
+-- -----------------------------------------------------
 -- Carga `usuari`
 -- -----------------------------------------------------
 INSERT INTO `usuari` VALUES ('1', 'mimail@yahoo.com', 'mimail09', '1990-12-31', NULL,'73', '08090');
@@ -166,8 +190,8 @@ INSERT INTO `estat_video` VALUES ('3', 'Privat');
 -- -----------------------------------------------------
 -- Carga `video`
 -- ----------------------------------------------------- 
-INSERT INTO `video` VALUES ('1', 'Gatitos en acción', 'Muchos gatitos haciendo cosas de gatos', '3', 'gatos.mp4', '3021', NULL,'2', '1', '0', '2', '2020-12-23 00:00:07', '1');
-INSERT INTO `video` VALUES ('2', 'Perritos en acción', 'Muchos perritos haciendo cosas de perros', '37', 'perros.mp4', '4444', NULL,'37', '1', '1', '1', '2021-01-23 12:00:07', '1');
+INSERT INTO `video` VALUES ('1', 'Gatitos en acción', 'Muchos gatitos haciendo cosas de gatos', '3', 'gatos.mp4', '3021', NULL,'2', '1', '0', '2020-12-23 00:00:07', '1','1');
+INSERT INTO `video` VALUES ('2', 'Perritos en acción', 'Muchos perritos haciendo cosas de perros', '37', 'perros.mp4', '4444', NULL,'37', '1', '1', '2021-01-23 12:00:07', '1','2');
 
 -- -----------------------------------------------------
 -- Carga `video_like`
@@ -178,13 +202,25 @@ INSERT INTO `video_like` VALUES ('2', '2', '0', '2021-03-20 15:14:00');
 -- -----------------------------------------------------
 -- Carga `etiqueta`
 -- ----------------------------------------------------- 
-INSERT INTO `etiqueta` VALUES ('1', '#animales', '1');
-INSERT INTO `etiqueta` VALUES ('2', '#gatos', '1');
+INSERT INTO `etiqueta` VALUES ('1', '#animales');
+INSERT INTO `etiqueta` VALUES ('2', '#gatos');
+
+-- -----------------------------------------------------
+-- Carga `video etiqueta`
+-- ----------------------------------------------------- 
+INSERT INTO `video_etiqueta` VALUES ('1', '1');
+INSERT INTO `video_etiqueta` VALUES ('1', '2');
 
 -- -----------------------------------------------------
 -- Carga `playlist`
 -- ----------------------------------------------------- 
 INSERT INTO `playlist` VALUES ('1', 'una playlist', '2020-02-02', '1', '1');
+
+-- -----------------------------------------------------
+-- Carga `video_playlist`
+-- ----------------------------------------------------- 
+INSERT INTO `video_playlist` VALUES ('1', '1');
+INSERT INTO `video_playlist` VALUES ('1', '2');
 
 -- -----------------------------------------------------
 -- Carga `canal`
@@ -212,6 +248,5 @@ INSERT INTO `comentari` VALUES ('2', 'fatal', '1', '2');
 -- ----------------------------------------------------- 
 INSERT INTO `comentari_like` VALUES ('1', '2');
 INSERT INTO `comentari_like` VALUES ('2', '1');
-
 
 
